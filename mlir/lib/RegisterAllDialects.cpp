7af905d93bf12e6a79cef30c633f4bbd74f6bb49
jumerckx <31353884+jumerckx@users.noreply.github.com>
2026-04-23 21:34:49 +0200

    initial PDLConstr dialect

--- mlir/lib/RegisterAllDialects.cpp
+++ mlir/lib/RegisterAllDialects.cpp
@@ -62,6 +62,7 @@
 #include "mlir/Dialect/OpenACC/OpenACC.h"
 #include "mlir/Dialect/OpenMP/OpenMPDialect.h"
 #include "mlir/Dialect/PDL/IR/PDL.h"
+#include "mlir/Dialect/PDLConstr/IR/PDLConstr.h"
 #include "mlir/Dialect/PDLInterp/IR/PDLInterp.h"
 #include "mlir/Dialect/Ptr/IR/PtrDialect.h"
 #include "mlir/Dialect/Quant/IR/Quant.h"
@@ -135,6 +136,7 @@ void mlir::registerAllDialects(DialectRegistry &registry) {
                   NVVM::NVVMDialect,
                   omp::OpenMPDialect,
                   pdl::PDLDialect,
+                  pdl_constr::PDLConstrDialect,
                   pdl_interp::PDLInterpDialect,
                   ptr::PtrDialect,
                   quant::QuantDialect,
